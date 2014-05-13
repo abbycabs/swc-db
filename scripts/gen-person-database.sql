ad5f2013f1c576d39192ba26232bc527f9f57550
Abigail Cabunoc <abigail.cabunoc@oicr.on.ca>
2014-05-13 08:44:33 -0400

    initial commit

---
+++ scripts/gen-person-database.sql
@@ -0,0 +1,11 @@
+-- The `Person` table is used to explain the most basic queries.
+create table Person(
+  personal text,
+  family	 text
+);
+
+insert into Person values('William',   'Dyer');
+insert into Person values('Frank',     'Pabodie');
+insert into Person values('Anderson',  'Lake');
+insert into Person values('Valentina', 'Roerich');
+insert into Person values('Frank',     'Danforth');
